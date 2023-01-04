e7317f2d7f8c2da1cd94f08670c5eee2741f93bb
oscarzapi <oscar_zapi@hotmail.com>
2023-01-04 03:34:18 -0500

    react app created

--- server/index.js
+++ server/index.js
@@ -5,10 +5,7 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import clientRoutes from './routes/client.js'
 import generalRoutes from './routes/general.js'
-import managementRoutes from './routes/management.js'
-import salesRoutes from './routes/sales.js'
 
 /*CONFIGURATION*/
 dotenv.config()
@@ -24,8 +21,13 @@ app.use(bodyParser.urlencoded({extended:false}))
 app.use(cors())
 
 /* ROUTES */
-app.use('/client', clientRoutes)
 app.use('/general', generalRoutes)
-app.use('/management', managementRoutes)
-app.use('/sales', salesRoutes)
 
+/* MONGOOSE SET UP */
+const PORT = process.env.PORT || 9000
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server port: ${PORT}`))
+}).catch(error => console.log(`${error} did not connect`))
